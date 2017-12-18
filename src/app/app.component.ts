dfc71753a0e244a77b291d239ca6adc2c615e42e
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-12-18 10:36:42 -0200

    Aula 5. Capitulo 10. Parcial inclusão dataTable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  lancamentos = [
+    {tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
+     dataPagamento: null, valor: 4.5, pessoa: 'Padaria do José'},
+    {tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '18/06/2017',
+     dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil'}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { TabViewModule } from 'primeng/primeng';
+import { TabViewModule, DataTableModule, ButtonModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
+
 
 
 @NgModule({
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    TabViewModule
+    TabViewModule,
+    DataTableModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
