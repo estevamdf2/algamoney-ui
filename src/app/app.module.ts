8b22283e2deb6a2b800be89fe0adb20f3819d652
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-12-18 10:03:37 -0200

    Inclusão do p-tabView

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { TabViewModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
