a1c91ba3aa637799ad9629fb10d8ff3fc8e4353c
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-12-20 09:51:02 -0200

    Cap 10. A10 - criando componente de pesquisa de lançamentos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  lancamentos = [
-    {tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
-     dataPagamento: null, valor: 4.5, pessoa: 'Padaria do José'},
-    {tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '18/06/2017',
-     dataPagamento: '09/06/2017', valor: 8000.00, pessoa: 'Atacado Brasil'},
-    {tipo: 'RECEITA', descricao: 'Provimentos', dataVencimento: '30/11/2017',
-     dataPagamento: '02/12/2017', valor: 4535.12, pessoa: 'EBC'},
-    {tipo: 'DESPESA', descricao: 'Vale transporte', dataVencimento: '02/12/2017',
-     dataPagamento: '02/12/2017', valor: 210, pessoa: 'df trans'}
-  ];
+   
 }
