a187517fb3a076aed29ea791492e0e888ff26acf
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-12-20 10:05:11 -0200

    Cap10. Aulas 10 e 11 - criando componente de pesquisa e barra menu

---
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamentos-pesquisa',
+  templateUrl: './lancamentos-pesquisa.component.html',
+  styleUrls: ['./lancamentos-pesquisa.component.css']
+})
+export class LancamentosPesquisaComponent {
+
+  lancamentos = [
+    {tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
+     dataPagamento: null, valor: 4.5, pessoa: 'Padaria do José'},
+    {tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '18/06/2017',
+     dataPagamento: '09/06/2017', valor: 8000.00, pessoa: 'Atacado Brasil'},
+    {tipo: 'RECEITA', descricao: 'Provimentos', dataVencimento: '30/11/2017',
+     dataPagamento: '02/12/2017', valor: 4535.12, pessoa: 'EBC'},
+    {tipo: 'DESPESA', descricao: 'Vale transporte', dataVencimento: '02/12/2017',
+     dataPagamento: '02/12/2017', valor: 210, pessoa: 'df trans'}
+  ];
+  
+}
+
+}
