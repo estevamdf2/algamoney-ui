002f5e231538aa586d80483061e91438fda7209b
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-12-20 09:28:25 -0200

    Cap10. A7 - paginação de dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,10 @@ export class AppComponent {
     {tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
      dataPagamento: null, valor: 4.5, pessoa: 'Padaria do José'},
     {tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '18/06/2017',
-     dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil'}
+     dataPagamento: '09/06/2017', valor: 8000.00, pessoa: 'Atacado Brasil'},
+    {tipo: 'RECEITA', descricao: 'Provimentos', dataVencimento: '30/11/2017',
+     dataPagamento: '02/12/2017', valor: 4535.12, pessoa: 'EBC'},
+    {tipo: 'DESPESA', descricao: 'Vale transporte', dataVencimento: '02/12/2017',
+     dataPagamento: '02/12/2017', valor: 210, pessoa: 'df trans'}
   ];
 }
