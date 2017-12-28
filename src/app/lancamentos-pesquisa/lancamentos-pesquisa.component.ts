a95b43f5382115920e194f55dffb91aca5f7c095
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-12-28 17:46:27 -0200

    Cap 11. Aula 8 - desafio uso do pipe

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -8,14 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class LancamentosPesquisaComponent {
 
   lancamentos = [
-    {tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
+    {tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2017, 5, 30),
      dataPagamento: null, valor: 4.5, pessoa: 'Padaria do José'},
-    {tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '18/06/2017',
-     dataPagamento: '09/06/2017', valor: 8000.00, pessoa: 'Atacado Brasil'},
-    {tipo: 'RECEITA', descricao: 'Provimentos', dataVencimento: '30/11/2017',
-     dataPagamento: '02/12/2017', valor: 4535.12, pessoa: 'EBC'},
-    {tipo: 'DESPESA', descricao: 'Vale transporte', dataVencimento: '02/12/2017',
-     dataPagamento: '02/12/2017', valor: 210, pessoa: 'df trans'}
+    {tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: new Date(2017, 6, 18),
+     dataPagamento: new Date(2017, 9, 9), valor: 8000.00, pessoa: 'Atacado Brasil'},
+    {tipo: 'RECEITA', descricao: 'Provimentos', dataVencimento: new Date(2017, 11, 30),
+     dataPagamento: new Date(2017, 12, 2) , valor: 4535.12, pessoa: 'EBC'},
+    {tipo: 'DESPESA', descricao: 'Vale transporte', dataVencimento: new Date(2017, 12, 2),
+     dataPagamento: new Date(2017, 12, 2), valor: 210, pessoa: 'df trans'}
   ];
 
 }
