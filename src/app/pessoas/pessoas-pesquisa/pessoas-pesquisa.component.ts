cb8347ea1ffff18b2b08e6cf13a27109ed295a7e
marcos <estevamdf@gmail.com>
2019-10-20 15:25:38 -0300

    Criação do error-handler

--- src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { PessoaService, PessoaFiltro } from '../pessoa.service';
-import { LazyLoadEvent } from 'primeng/primeng';
+import { LazyLoadEvent, ConfirmationService } from 'primeng/primeng';
+import { ErrorHandlerService } from '../../core/error-handler.service';
+import { ToastyService } from 'ng2-toasty';
 
 @Component({
   selector: 'app-pessoas-pesquisa',
@@ -12,10 +14,14 @@ export class PessoasPesquisaComponent {
   totalRegistros = 0;
   filtro = new PessoaFiltro();
   pessoas = [];
+  @ViewChild('tabela') grid;
 
-  constructor(private pessoaService: PessoaService){
-    
-  }
+  constructor(
+    private pessoaService: PessoaService,
+    private errorHandler: ErrorHandlerService,
+    private toasty: ToastyService,
+    private confirmation: ConfirmationService
+  ){ }
 
   pesquisar(pagina = 0){
 
@@ -31,6 +37,32 @@ export class PessoasPesquisaComponent {
   aoMudarPagina(event: LazyLoadEvent){     
     const pagina = event.first / event.rows;
     this.pesquisar(pagina);
+  }
+
+  confirmarExclusao(pessoa: any){
+
+    this.confirmation.confirm({
+      message: 'Tem certeza que deseja excluir '+pessoa.nome,
+      accept: () =>{
+        this.excluir(pessoa);
+      }
+    });
+  }
+
+  excluir(pessoa: any){
+    this.pessoaService.excluir(pessoa.codigo)
+      .then(() =>{
+        console.log('Excluindo pessoa... ');
+
+        if(this.grid.first === 0){
+          this.pesquisar();
+        } else{
+          this.grid.first = 0;
+        }
+
+        this.toasty.success('Pessoa excluída com sucesso.');
+      })
+      .catch(erro => this.errorHandler.handle(erro));
   }
 
 }
