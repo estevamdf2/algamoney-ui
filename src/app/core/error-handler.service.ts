f6eced8f03c1d14222110758364fa43f7b87d84d
marcos <estevamdf@gmail.com>
2019-10-20 16:01:00 -0300

    Cap 17. A14 minha resolução desafio

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ToastyService } from 'ng2-toasty';
+import { errorHandler } from '@angular/platform-browser/src/browser';
+
 
 @Injectable()
 export class ErrorHandlerService {
@@ -8,6 +10,9 @@ export class ErrorHandlerService {
 
   handle(errorResponse: any){
     let msg: string;
+    console.log('status', errorResponse.status);
+    console.log('body', errorResponse.body);
+
 
     if (typeof errorResponse === 'string'){
       msg = errorResponse;
@@ -15,6 +20,15 @@ export class ErrorHandlerService {
       msg = 'Erro ao processar um serviço.'
       console.log('Ocorreu um erro',errorResponse);
     }
+
+    if( errorResponse.status > 400 || errorResponse.status <= 499 ){
+      console.log('status 4xx');
+      var json = JSON.parse(errorResponse._body);
+      msg = json[0].mensagemUsuario;
+      
+    }
+
+    
 
     this.toast.error(msg);
   }
