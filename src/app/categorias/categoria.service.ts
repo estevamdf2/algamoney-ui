538f0d0e7ae687dd41d9e51ebbd8160e8d8ba20c
Marcos Sousa <marcos.sousa@ebc.com.br>
2019-10-23 14:13:01 -0300

    Cap 17. A 17 dropdown categorias.

--- src/app/categorias/categoria.service.ts
+++ src/app/categorias/categoria.service.ts
@@ -15,15 +15,11 @@ export class CategoriaService {
 
     listarTodas(): Promise<any> {        
         const headers = new Headers();
-        headers.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCJdLCJub21lIjoiQWRtaW5pc3RyYWRvciIsImV4cCI6MTU3MTc5OTk0MCwiYXV0aG9yaXRpZXMiOlsiUk9MRV9DQURBU1RSQVJfQ0FURUdPUklBIiwiUk9MRV9QRVNRVUlTQVJfUEVTU09BIiwiUk9MRV9SRU1PVkVSX1BFU1NPQSIsIlJPTEVfQ0FEQVNUUkFSX0xBTkNBTUVOVE8iLCJST0xFX1BFU1FVSVNBUl9MQU5DQU1FTlRPIiwiUk9MRV9SRU1PVkVSX0xBTkNBTUVOVE8iLCJST0xFX0NBREFTVFJBUl9QRVNTT0EiLCJST0xFX1BFU1FVSVNBUl9DQVRFR09SSUEiXSwianRpIjoiZjIwNGM4NTItMWMxNi00ODViLTlkMzUtMmMwZTJiNzIyOWMwIiwiY2xpZW50X2lkIjoibW9iaWxlIn0.WTN8g-rzeJBeW4PP4cuHbXkfehlXgLCCD7qaGGpImH4');
+        headers.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCJdLCJub21lIjoiQWRtaW5pc3RyYWRvciIsImV4cCI6MTU3MTg1MjIyMiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9DQURBU1RSQVJfQ0FURUdPUklBIiwiUk9MRV9QRVNRVUlTQVJfUEVTU09BIiwiUk9MRV9SRU1PVkVSX1BFU1NPQSIsIlJPTEVfQ0FEQVNUUkFSX0xBTkNBTUVOVE8iLCJST0xFX1BFU1FVSVNBUl9MQU5DQU1FTlRPIiwiUk9MRV9SRU1PVkVSX0xBTkNBTUVOVE8iLCJST0xFX0NBREFTVFJBUl9QRVNTT0EiLCJST0xFX1BFU1FVSVNBUl9DQVRFR09SSUEiXSwianRpIjoiNTExZTZkYzgtMDgwYi00OWI2LWIzMmEtYTllMzNlNGRjZDI3IiwiY2xpZW50X2lkIjoibW9iaWxlIn0.zHQ1hJC4fO7JPS6BHOatHxBxzvIVs5UUkM3SIh60CBM');
 
-        return this.http.get(`${this.categoriaUrl}`, {headers})
+        return this.http.get(this.categoriaUrl, {headers})
             .toPromise()
-            .then(response =>{
-                const categorias = response.json();
-                console.log('categorias ',categorias);
-                return categorias;
-            })
+            .then(response => response.json());
     }
 
 }
