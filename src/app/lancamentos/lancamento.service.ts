eddf6474ea0d4ae220592a44d3e483ad21d68823
marcos <estevamdf@gmail.com>
2019-11-04 23:25:56 -0300

    Cap. 18. A6. Erro no desafio

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Http , Headers, URLSearchParams} from '@angular/http';
+import { Http, Headers, URLSearchParams } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
-import * as moment from 'moment';
 import { Lancamento } from '../core/model';
+import * as moment from 'moment';
+
 
 export class LancamentoFiltro {
   descricao: string;
@@ -15,39 +16,39 @@ export class LancamentoFiltro {
 @Injectable()
 export class LancamentoService {
 
-  lancamentosUrl= 'http://localhost:8080/lancamentos';
-  
+  lancamentosUrl = 'http://localhost:8080/lancamentos';
+
 
   constructor(private http: Http) { }
 
-  pesquisar(filtro: LancamentoFiltro) :Promise<any> {    
+  pesquisar(filtro: LancamentoFiltro): Promise<any> {
 
     const params = new URLSearchParams();
     const headers = new Headers();
-    headers.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCJdLCJub21lIjoiQWRtaW5pc3RyYWRvciIsImV4cCI6MTU3MTg1MjIyMiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9DQURBU1RSQVJfQ0FURUdPUklBIiwiUk9MRV9QRVNRVUlTQVJfUEVTU09BIiwiUk9MRV9SRU1PVkVSX1BFU1NPQSIsIlJPTEVfQ0FEQVNUUkFSX0xBTkNBTUVOVE8iLCJST0xFX1BFU1FVSVNBUl9MQU5DQU1FTlRPIiwiUk9MRV9SRU1PVkVSX0xBTkNBTUVOVE8iLCJST0xFX0NBREFTVFJBUl9QRVNTT0EiLCJST0xFX1BFU1FVSVNBUl9DQVRFR09SSUEiXSwianRpIjoiNTExZTZkYzgtMDgwYi00OWI2LWIzMmEtYTllMzNlNGRjZDI3IiwiY2xpZW50X2lkIjoibW9iaWxlIn0.zHQ1hJC4fO7JPS6BHOatHxBxzvIVs5UUkM3SIh60CBM');
+    headers.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCJdLCJub21lIjoiQWRtaW5pc3RyYWRvciIsImV4cCI6MTU3MjkyMTIzNSwiYXV0aG9yaXRpZXMiOlsiUk9MRV9DQURBU1RSQVJfQ0FURUdPUklBIiwiUk9MRV9QRVNRVUlTQVJfUEVTU09BIiwiUk9MRV9SRU1PVkVSX1BFU1NPQSIsIlJPTEVfQ0FEQVNUUkFSX0xBTkNBTUVOVE8iLCJST0xFX1BFU1FVSVNBUl9MQU5DQU1FTlRPIiwiUk9MRV9SRU1PVkVSX0xBTkNBTUVOVE8iLCJST0xFX0NBREFTVFJBUl9QRVNTT0EiLCJST0xFX1BFU1FVSVNBUl9DQVRFR09SSUEiXSwianRpIjoiZTFlOWU0NzctMDE4My00MGVlLWIwYTItODMyNGE0MDQ1Y2Q0IiwiY2xpZW50X2lkIjoibW9iaWxlIn0.zRkZrhmJn7qubQFC6-WnKmPoSNY3fNGDnamSlBpmEXI');
 
-    params.set('page',filtro.pagina.toString());
+    params.set('page', filtro.pagina.toString());
     params.set('size', filtro.itensPorPagina.toString());
-    
-    if(filtro.descricao){
+
+    if (filtro.descricao) {
       params.set('descricao', filtro.descricao);
     }
 
-    if(filtro.dataVencimentoInicio){
-      params.set('dataVencimentoDe', 
+    if (filtro.dataVencimentoInicio) {
+      params.set('dataVencimentoDe',
         moment(filtro.dataVencimentoInicio).format("YYYY-MM-DD"));
     }
 
-    if(filtro.dataVencimentoFim){
-      params.set('dataVencimentoAte', 
+    if (filtro.dataVencimentoFim) {
+      params.set('dataVencimentoAte',
         moment(filtro.dataVencimentoFim).format("YYYY-MM-DD"));
     }
 
-    console.log('params ',params);
+    console.log('params ', params);
     return this.http.get(`${this.lancamentosUrl}?resumo`,
       { headers, search: params })
       .toPromise()
-      .then(response => {        
+      .then(response => {
         const responseJson = response.json();
         const lancamentos = responseJson.content;
 
@@ -60,13 +61,13 @@ export class LancamentoService {
       });
   }
 
-  excluir(codigo: number):Promise<void>{
+  excluir(codigo: number): Promise<void> {
 
-    console.log('codigo ',codigo);
+    console.log('codigo ', codigo);
     const headers = new Headers();
     headers.append('Authorization', 'Bearer ddeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCJdLCJub21lIjoiQWRtaW5pc3RyYWRvciIsImV4cCI6MTU3MTU5MzM1NSwiYXV0aG9yaXRpZXMiOlsiUk9MRV9DQURBU1RSQVJfQ0FURUdPUklBIiwiUk9MRV9QRVNRVUlTQVJfUEVTU09BIiwiUk9MRV9SRU1PVkVSX1BFU1NPQSIsIlJPTEVfQ0FEQVNUUkFSX0xBTkNBTUVOVE8iLCJST0xFX1BFU1FVSVNBUl9MQU5DQU1FTlRPIiwiUk9MRV9SRU1PVkVSX0xBTkNBTUVOVE8iLCJST0xFX0NBREFTVFJBUl9QRVNTT0EiLCJST0xFX1BFU1FVSVNBUl9DQVRFR09SSUEiXSwianRpIjoiOGIyODliOGItNjYxYy00ODcxLThmYzUtODdkZWZhMzQyMWExIiwiY2xpZW50X2lkIjoibW9iaWxlIn0.UisB0kMA4Q0CNMhnTSROouT4KJqbiHhST7bCjo15Eus');
-    
-    return this.http.delete(`${this.lancamentosUrl}/${codigo}`, { headers})
+
+    return this.http.delete(`${this.lancamentosUrl}/${codigo}`, { headers })
       .toPromise()
       .then(() => null);
 
@@ -76,12 +77,59 @@ export class LancamentoService {
 
     const headers = new Headers();
     headers.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCJdLCJub21lIjoiQWRtaW5pc3RyYWRvciIsImV4cCI6MTU3MjAyNDE3NiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9DQURBU1RSQVJfQ0FURUdPUklBIiwiUk9MRV9QRVNRVUlTQVJfUEVTU09BIiwiUk9MRV9SRU1PVkVSX1BFU1NPQSIsIlJPTEVfQ0FEQVNUUkFSX0xBTkNBTUVOVE8iLCJST0xFX1BFU1FVSVNBUl9MQU5DQU1FTlRPIiwiUk9MRV9SRU1PVkVSX0xBTkNBTUVOVE8iLCJST0xFX0NBREFTVFJBUl9QRVNTT0EiLCJST0xFX1BFU1FVSVNBUl9DQVRFR09SSUEiXSwianRpIjoiNzMxYmJlMjMtZmNjZC00MTEzLTgzM2ItY2IyYzU1MWU4NjExIiwiY2xpZW50X2lkIjoibW9iaWxlIn0.GTfWEHGTERRfAhRRFtPv5d4W0TlJXp3U7u9Ao22ge5I');
-    headers.append('Content-Type', 'application/json');    
+    headers.append('Content-Type', 'application/json');
 
-    console.log('lancamento ',lancamento);
-    return this.http.post(this.lancamentosUrl, JSON.stringify(lancamento), {headers})
+    console.log('lancamento ', lancamento);
+    return this.http.post(this.lancamentosUrl, JSON.stringify(lancamento), { headers })
       .toPromise()
       .then(response => response.json());
 
   }
+
+  atualizar(lancamento: Lancamento): Promise<Lancamento> {
+    console.log('atualizar');
+    const headers = new Headers();
+    headers.append('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCJdLCJub21lIjoiQWRtaW5pc3RyYWRvciIsImV4cCI6MTU3MjkyMTIzNSwiYXV0aG9yaXRpZXMiOlsiUk9MRV9DQURBU1RSQVJfQ0FURUdPUklBIiwiUk9MRV9QRVNRVUlTQVJfUEVTU09BIiwiUk9MRV9SRU1PVkVSX1BFU1NPQSIsIlJPTEVfQ0FEQVNUUkFSX0xBTkNBTUVOVE8iLCJST0xFX1BFU1FVSVNBUl9MQU5DQU1FTlRPIiwiUk9MRV9SRU1PVkVSX0xBTkNBTUVOVE8iLCJST0xFX0NBREFTVFJBUl9QRVNTT0EiLCJST0xFX1BFU1FVSVNBUl9DQVRFR09SSUEiXSwianRpIjoiZTFlOWU0NzctMDE4My00MGVlLWIwYTItODMyNGE0MDQ1Y2Q0IiwiY2xpZW50X2lkIjoibW9iaWxlIn0.zRkZrhmJn7qubQFC6-WnKmPoSNY3fNGDnamSlBpmEXI');
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.put(`${this.lancamentosUrl}/${lancamento.codigo}`,
+        JSON.stringify(lancamento), { headers })
+      .toPromise()
+      .then(response => {
+        const lancamentoAlterado = response.json() as Lancamento;
+
+        this.converterStringsParaDatas([lancamentoAlterado]);
+
+        return lancamentoAlterado;
+      });
+  }
+
+  buscarPorCodigo(codigo: number): Promise<Lancamento> {
+    const headers = new Headers();
+    headers.append('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCJdLCJub21lIjoiQWRtaW5pc3RyYWRvciIsImV4cCI6MTU3MjkyMTIzNSwiYXV0aG9yaXRpZXMiOlsiUk9MRV9DQURBU1RSQVJfQ0FURUdPUklBIiwiUk9MRV9QRVNRVUlTQVJfUEVTU09BIiwiUk9MRV9SRU1PVkVSX1BFU1NPQSIsIlJPTEVfQ0FEQVNUUkFSX0xBTkNBTUVOVE8iLCJST0xFX1BFU1FVSVNBUl9MQU5DQU1FTlRPIiwiUk9MRV9SRU1PVkVSX0xBTkNBTUVOVE8iLCJST0xFX0NBREFTVFJBUl9QRVNTT0EiLCJST0xFX1BFU1FVSVNBUl9DQVRFR09SSUEiXSwianRpIjoiZTFlOWU0NzctMDE4My00MGVlLWIwYTItODMyNGE0MDQ1Y2Q0IiwiY2xpZW50X2lkIjoibW9iaWxlIn0.zRkZrhmJn7qubQFC6-WnKmPoSNY3fNGDnamSlBpmEXI');
+
+    return this.http.get(`${this.lancamentosUrl}/${codigo}`, { headers })
+      .toPromise()
+      .then(response => {
+        const lancamento = response.json() as Lancamento;
+
+        this.converterStringsParaDatas([lancamento]);
+
+        return lancamento;
+      });
+  }
+
+  private converterStringsParaDatas(lancamentos: Lancamento[]) {
+    for (const lancamento of lancamentos) {
+      lancamento.dataVencimento = moment(lancamento.dataVencimento,
+        'YYYY-MM-DD').toDate();
+
+      if (lancamento.dataPagamento) {
+        lancamento.dataPagamento = moment(lancamento.dataPagamento,
+          'YYYY-MM-DD').toDate();
+      }
+    }
+  }
+
 }
+
